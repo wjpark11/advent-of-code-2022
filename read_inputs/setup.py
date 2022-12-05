2c68150a590e1296a6873cfd73ade4469890561d
Wonjin Park <wjpark11@gmail.com>
2022-12-05 19:30:13 +0900

    update readinputs

--- read_inputs/setup.py
+++ read_inputs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="readinputs",
-    version="1.0.0",
+    version="1.0.1",
     description="read input module for advent of code",
     author="wjpark",
     author_email="wjpark11@gmail.com",
